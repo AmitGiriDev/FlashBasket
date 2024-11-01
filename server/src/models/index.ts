1901cfdbd60a9045eb7e2c15768caa4ea2085ba2
Amit Giri <thisisag840@gmail.com>
2024-11-01 20:17:34 +0530

    adding orders and counter

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,4 +2,6 @@ import { Branch } from "./branch.ts";
 import { Customer, DeliveryPartner, Admin } from "./user.ts";
 import Category from "./category.ts";
 import Product from "./products.ts";
-export { Customer, DeliveryPartner, Admin, Branch, Product, Category };
+import Counter from "./counter.ts";
+
+export { Customer, DeliveryPartner, Admin, Branch, Product, Category, Counter };
