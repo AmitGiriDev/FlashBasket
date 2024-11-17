be5764dc49854e2ebfa845dfeb348d28c76e4dd3
Amit Giri <thisisag840@gmail.com>
2024-11-17 17:29:50 +0530

    gesture handle to navigate to delivery loggin

--- flashbasket/src/features/auth/CustomerLogin.tsx
+++ flashbasket/src/features/auth/CustomerLogin.tsx
@@ -1,14 +1,54 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React, {FC} from 'react';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {View, Text, StyleSheet, Animated} from 'react-native';
+import React, {FC, useState} from 'react';
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  State,
+} from 'react-native-gesture-handler';
 import CustomSafeAreaView from '@components/global/CustomSafeAreaView';
 import ProductSlider from '@components/login/ProductSlider';
+import {resetAndNavigate} from '@utils/Navigation.utils';
+import {DELIVERY_LOGIN_SCREEN} from '@navigation/ScreenNames';
 
 const CustomerLogin: FC = () => {
+  const [gesturePattern, setGesturePattern] = useState<String[]>([]);
+
+  const handleGesture = ({nativeEvent}: any) => {
+    if (nativeEvent.state === State.END) {
+      const {translationX, translationY} = nativeEvent;
+      let direction = '';
+
+      if (Math.abs(translationX) > Math.abs(translationY)) {
+        //horizontal scroll
+        direction = translationX > 0 ? 'right' : 'left';
+      } else {
+        //vertical scroll
+        direction = translationY < 0 ? 'up' : 'down';
+      }
+      console.log(translationX, translationY, direction);
+
+      const newSequence = [...gesturePattern, direction].slice(-2);
+
+      setGesturePattern(newSequence);
+
+      if (newSequence.join(' ') === 'up up') {
+        setGesturePattern([]);
+        resetAndNavigate(DELIVERY_LOGIN_SCREEN);
+      }
+    }
+  };
+
   return (
     <GestureHandlerRootView style={styles.container}>
-      <CustomSafeAreaView>
+      <CustomSafeAreaView style={styles.container}>
         <ProductSlider />
+        <PanGestureHandler onHandlerStateChange={handleGesture}>
+          <Animated.ScrollView
+            bounces={false}
+            keyboardShouldPersistTaps="handled"
+            keyboardDismissMode={'on-drag'}
+            contentContainerStyle={styles.subContainer}></Animated.ScrollView>
+        </PanGestureHandler>
       </CustomSafeAreaView>
     </GestureHandlerRootView>
   );
@@ -19,5 +59,11 @@ export default CustomerLogin;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  subContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 20,
   },
 });
