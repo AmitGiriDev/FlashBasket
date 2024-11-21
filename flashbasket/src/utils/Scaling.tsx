558c068d124a29de7862cd49f40124fe6f9258f5
Amit Giri <thisisag840@gmail.com>
2024-11-21 23:27:49 +0530

    updated utils

--- flashbasket/src/utils/Scaling.tsx
+++ flashbasket/src/utils/Scaling.tsx
@@ -1,4 +1,9 @@
-import {Dimensions} from 'react-native';
+import {Dimensions, Platform} from 'react-native';
 
 export const screeWidth: number = Dimensions.get('window').width;
 export const screeHeight: number = Dimensions.get('window').height;
+
+export const NoticeHeight =
+  Platform.OS === 'ios' ? screeHeight * 0.12 : screeHeight * 0.07;
+export const NoticeWidth =
+  Platform.OS === 'ios' ? screeWidth * 0.12 : screeWidth * 0.07;
